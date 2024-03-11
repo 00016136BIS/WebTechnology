ad2cd6648941e2a2e30e93a852345b7377b7199f
00016136-wiut <00016136BIS@wiut.uz>
2024-03-11 17:16:10 +0500

    my first init commit

---
+++ app.js
@@ -0,0 +1,19 @@
+const conf = require ('dotenv').config
+
+const express = require ('express')
+
+
+const app = express()
+const port = process.env.port || 3000
+
+app.get ('/', (req,res)=>{
+    return res.status(200).json({
+        message: "Hello world"
+    })
+})
+
+app.listen(port, ()=>{
+    console.log("server running at port:" + port)
+}
+
+)
